4c50ae7aff091eaeef83808ad599d881e94cd37e
Lucas Hermes Felini <lucashermes92@gmail.com>
2022-07-25 22:52:04 -0300

    Criando variavéis e constante para o exercício

--- 01-fundamentos/bloco-04-introducao-javascript/dia-01-02-tipos-primitivos-tipagem-operacoes-aritimeticas/scripts.js
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-01-02-tipos-primitivos-tipagem-operacoes-aritimeticas/scripts.js
@@ -1,12 +1,17 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-    firstName: 'Ana',
-    lasName: 'Santos'
-};
-const patientEmail = 'ana@email.com';
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//     firstName: 'Ana',
+//     lasName: 'Santos'
+// };
+// const patientEmail = 'ana@email.com';
 
-console.log(typeof patientAge);
-console.log(typeof patientId);
-console.log(typeof isEnrolled);
-console.log(typeof patientInfo);
+// console.log(typeof patientAge);
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+
+const base = 5;
+let height = 8;
+const area;
+
